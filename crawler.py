6372ac6cf09e9372816471913a5c63772a2e9b8f
makoeppel <koeppel.ma@googlemail.com>
2015-11-13 15:05:19 +0100

    Update crawler.py
    add commends

--- crawler.py
+++ crawler.py
@@ -7,28 +7,28 @@ import socket
 
 
 
-def crawler(row,output):
-            global string
-            url = "http://" + row[3];
+def crawler(row,output):#Funktion zur Webseitabfrage
+            global string #Error Variable
+            url = "http://" + row[3]; #Url der ausgewaehlten Website; row je nach MySQL Datenbank
             string = "noError"
-            try:
+            try: #try to open Website
                 content = urllib2.urlopen(url, None, timeout = 10).read()
-            except urllib2.HTTPError, e:
+            except urllib2.HTTPError, e: #Errors wie timeout, Passwortabfrage, nicht verfuegbar
                 string = str(e.code)
                 content = "noCode"
-            except urllib2.URLError, e:
+            except urllib2.URLError, e: #Errors auf URL fehler
                 content = "noCode"
                 string = str(e.args)
-            except socket.timeout, e:
+            except socket.timeout, e: #socket timeout Error
                 content = "noCode"
                 string = "timeout"
             
             output.write(url);
-            if re.search("wfxbuild",content):
+            if re.search("wfxbuild",content): #wsb ueberpruefung im Quelltext
                 output.write("; wfxja ");
             else:
                 output.write("; wfxnein ");
-            if re.search("webfix",content):
+            if re.search("webfix",content): #wsc ueberpruefung im Quelltext
                 output.write("; webja; "+string);
                 output.write("\n");
             else:
